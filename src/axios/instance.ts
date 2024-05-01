c1d567286590329f53bf045bc799b207af30c05e
Adrian Rodriguez <83842205+2piradrian@users.noreply.github.com>
2024-04-30 21:31:53 -0300

    baseurl

--- src/axios/instance.ts
+++ src/axios/instance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-	baseURL: "https://recipes-app-backend-ts.onrender.com",
-	//baseURL: "http://localhost:3333",
+	baseURL: "http://localhost:3333",
 });
